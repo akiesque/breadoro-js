d921a4254ae828304b8f24ae399f4cdfe6b4f508
Mimi <mimi.artbox@gmail.com>
2026-06-29 15:18:49 +0800

    Added small UI

--- scripts/timer.js
+++ scripts/timer.js
@@ -1,32 +1,30 @@
 let isPaused = false;
 let breakTime = false;
 let timer;
+let pomoTimer;
 
 
-let minute, second;
-let minutes, seconds;
+let minute;
+let minutes;
 
-let sessions, breaks, settings;
+let sessions, breaks, settings, sessionsDone;
 let notify;
 
 function startTimer() {
     isPaused = false;
-    const timerContainer = document.querySelector('.timer-container');
+    pomoTimer = document.querySelector('.pomo-time');
     notify = document.querySelector('.notify-text');
     minute = document.querySelector('#minutes');
-    second = document.querySelector('#seconds');
     settings = document.querySelector('.settings-container');
 
     sessions = Number(document.querySelector('#sessions').value) || 4;
     breaks = Number(document.querySelector('#breaks').value) || 5;
 
-    seconds = Number(second.value) > 59 ? 59 : Number(second.value) % 60;
-     minutes = Number(minute.value) > 59 ? 59 : Number(minute.value) % 60 || seconds > 0 ? 0 : 25;
+    seconds = 0;
+    minutes = Number(minute.value) > 59 ? 59 : Number(minute.value) % 60 || 25;
     
-    document.querySelector('.input-container').classList.add('hidden');
     document.querySelector('.settings-container').classList.add('hidden');
-    document.querySelector('.timer-container').classList.remove('hidden');
-    timerContainer.innerHTML = `<p>${timerFormat(minutes, seconds)}</p>`;
+    pomoTimer.innerHTML = `<p>${timerFormat(minutes)}</p>`;
 
     updateTimer();
     timer = setInterval(() => {updateTimer()}, 1000);
@@ -40,8 +38,8 @@ function timerFormat(m, s) {
 }
 
 function updateTimer() {
-    const timerContainer = document.querySelector('.timer-container');
-    timerContainer.innerHTML = `<p>${timerFormat(minutes, seconds)}</p>`;
+    const pomoTimer = document.querySelector('.pomo-time');
+    pomoTimer.innerHTML = `${timerFormat(minutes, seconds)}`;
 
     if (minutes > 0 && seconds === 0) {
         if (breakTime) {
@@ -61,16 +59,19 @@ function updateTimer() {
         // timerContainer.innerHTML = `<p>${timerFormat(minutes, seconds)}</p>`;
         } else {
             breakTime = false;
-
             sessions > 0 ? sessions-- : 0;
-            if (sessions === 0) {
+            sessionsDone += 1;
+            if (sessions === 0 && sessionsDone === sessions) {
                 clearInterval(timer);
                 notify.innerHTML = "All sessions complete! Good job!";
             } else {
-            minutes = Number(minute.value) > 59 ? 59 : Number(minute.value) % 60;
-            seconds = Number(second.value) > 59 ? 59 : Number(second.value) % 60;
-            timerContainer.innerHTML = `<p>${timerFormat(minutes, seconds)}</p>`;
-            notify.innerHTML = "Break complete! Let's start the next session.";
+                if (sessions > 0 &&sessionsDone === 4) {
+                    minutes = breaks === 5 ? 30 : breaks + minutes;
+                    seconds = Number(second.value) > 59 ? 59 : Number(second.value) % 60;
+                    timerContainer.innerHTML = `<p>${timerFormat(minutes, seconds)}</p>`;
+                    notify.innerHTML = "Break complete! Let's start the next session.";
+                }
+            
             }   
         }
     }
@@ -96,10 +97,8 @@ function toggleTimerButton() {
 
 function restartTimer() {
     clearInterval(timer);
-    document.querySelector('.input-container').classList.remove('hidden');
     document.querySelector('.settings-container').classList.remove('hidden');
     document.querySelector('.settings-display').classList.add('hidden');
-    document.querySelector('.timer-container').classList.add('hidden');
     minutes = 0;
     seconds = 0;
     sessions = 0;
@@ -116,5 +115,19 @@ function displaySettings() {
     const displaySessions = document.querySelector('#display-sessions');
     const displayBreaks = document.querySelector('#display-breaks');
     displaySessions.innerHTML = `Sessions: ${sessions}`;
-    displayBreaks.innerHTML = `Breaks: ${breaks}`;
+    displayBreaks.innerHTML = `Breaks: ${breaks} minutes`;
+}
+
+function openSettings() {
+    const settingsScreen = document.querySelector('#settings-screen');
+    settingsScreen.classList.remove('hidden');
+}
+
+function closeSettings() {
+    const settingsScreen = document.querySelector('#settings-screen');
+    settingsScreen.classList.add('hidden');
+    const min = Number(document.querySelector('#minutes').value)
+    minutes = min;
+    seconds = 0;
+    document.querySelector('.pomo-time').innerHTML = `${timerFormat(minutes, seconds)}`;
 }
